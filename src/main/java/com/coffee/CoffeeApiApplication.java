415e50e951262da93ef7cdc8b0ad9e29631af518
tanFangJie <1023369905@qq.com>
2025-03-15 15:36:37 +0800

    初始化mybatis-plus配置和数据库配置

--- src/main/java/com/coffee/CoffeeApiApplication.java
+++ src/main/java/com/coffee/CoffeeApiApplication.java
@@ -2,8 +2,11 @@ package com.coffee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class CoffeeApiApplication {
 
     public static void main(String[] args) {
